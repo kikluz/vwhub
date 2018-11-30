ba417ad347ffaffdb763b8612c1864ee07483c89
kikluz <kikluzr@gmail.com>
2018-11-30 14:25:00 -0800

    add ne wocde

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 	def index
+		@posts = Post.all
 	end
 
 	def new
@@ -7,7 +8,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		@post = Post.create(post_params)
+		@posts = Post.create(post_params)
 		redirect_to posts_path
 	end
 
