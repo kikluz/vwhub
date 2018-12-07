1aeeea77915e8f9ea92e2e28854bdc68a623ad53
kikluz <kikluzr@gmail.com>
2018-12-07 15:54:34 -0800

    add new code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
 	  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
+	   # ask yourself if the user should be told something once that specific action is complete
+	   # Create the flash message using the following code and insert it just prior to the redirect or render.
+	   #  If using the render method, adjust the flash message to flash.now:
+	   # flash[:success] = “Your personalised message here”
+	   # flash[:alert] = “Oh god something is wrong”
+
   def index
     @posts = Post.all
   end
