8309f68502b16233a22978d9b8edac88d90733f9
kikluz <savir81@hotmail.com>
2020-05-24 01:22:49 -0700

    config pg gem delete sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_29_210403) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "caption"
     t.datetime "created_at", null: false
